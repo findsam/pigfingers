7628a7ed269b604ff86e139e09f5f429396ba8d0
samsvk <samsvk00@gmail.com>
2022-04-24 15:48:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-function App() {
+function RenderQuote({ input, quote }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {quote.map((item, index) => {
+        let color;
+        if (index < input.length) {
+          color = item === input[index] ? "#99cc00" : "#ff2b33";
+        }
+        return (
+          <span style={{ color: color }} key={index}>
+            {item}
+          </span>
+        );
+      })}
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  const [quote, setQuote] = React.useState("");
+  const [input, setInput] = React.useState("");
+
+  async function getQuote() {
+    const res = await fetch("https://api.quotable.io/random");
+    const { content } = await res.json();
+    setQuote(content);
+  }
+
+  React.useEffect(() => {
+    getQuote();
+  }, []);
+
+  if (quote.length === 0) return null; // small error handler.
+
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <RenderQuote input={input.split("")} quote={quote.split("")} />
+        <br /> <br />
+        <input
+          className="input"
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
