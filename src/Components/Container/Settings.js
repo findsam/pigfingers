cce93f237788029160bffcc619c982a19ccd81d7
samsvk <samsvk00@gmail.com>
2022-04-28 22:29:13 +0100

    working settings

--- src/Components/Container/Settings.js
+++ src/Components/Container/Settings.js
@@ -5,8 +5,46 @@ import { SiLetterboxd } from "react-icons/si";
 import { IoMdQuote } from "react-icons/io";
 import { FaClock, FaTachometerAlt, FaBars } from "react-icons/fa";
 
+function renderCorrectSetting(activeSetting, setActiveSetting) {
+  switch (activeSetting) {
+    case "test": {
+      return "hi";
+    }
+    default:
+      return (
+        <ul>
+          <li onClick={() => setActiveSetting("test")}>
+            <FaBars /> Mode...
+          </li>
+          <li>
+            <SiLetterboxd /> Words...
+          </li>
+          <li>
+            <IoMdQuote /> Quote length...
+          </li>
+          <li>
+            <FaClock /> Letter Progress...
+          </li>
+          <li>
+            <FaClock /> Word Progress...
+          </li>
+          <li>
+            <FaClock /> Time...
+          </li>
+          <li>
+            <FaTachometerAlt /> Live WPM...
+          </li>
+          <li>
+            <AiFillInfoCircle /> Creator...
+          </li>
+        </ul>
+      );
+  }
+}
+
 export default function Settings() {
   const [open, setOpen] = React.useState();
+  const [activeSetting, setActiveSetting] = React.useState();
 
   const close = React.useCallback((event) => {
     if (event.keyCode === 27) {
@@ -25,32 +63,7 @@ export default function Settings() {
     open && (
       <div className="settings">
         <div className="settings__inner">
-          <ul>
-            <li>
-              <FaBars /> Mode...
-            </li>
-            <li>
-              <SiLetterboxd /> Words...
-            </li>
-            <li>
-              <IoMdQuote /> Quote length...
-            </li>
-            <li>
-              <FaClock /> Letter Progress...
-            </li>
-            <li>
-              <FaClock /> Word Progress...
-            </li>
-            <li>
-              <FaClock /> Time...
-            </li>
-            <li>
-              <FaTachometerAlt /> Live WPM...
-            </li>
-            <li>
-              <AiFillInfoCircle /> Creator...
-            </li>
-          </ul>
+          {renderCorrectSetting(activeSetting, setActiveSetting)}
         </div>
       </div>
     )
