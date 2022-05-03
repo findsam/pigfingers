c9f16be3475121f7a18929c25aa268f1ce5a6f83
samsvk <samsvk00@gmail.com>
2022-05-03 05:09:14 +0100

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,49 @@
+// import Logo from "../Static/Sound/logo.png";
+import { FaTwitter, FaGithub, FaHeart } from "react-icons/fa";
+import { SiDiscord } from "react-icons/si";
+
+const ExternalLink = ({ href, children }) => (
+  <a target="_blank" rel="noopener noreferrer" href={href}>
+    {children}
+  </a>
+);
+
 export default function Header(props) {
   return (
     <header className={`header`}>
-      <aside className={`header__item header__item--left`}>Pigfingers</aside>
+      <aside className={`header__item header__item--left`}>
+        {/* <img src={Logo} alt="Pigfingers logo" /> */}
+      </aside>
       <aside
         className={`header__item header__item--right ${
           props.playing && "close"
         }`}
       >
         <ul className="header__item__ul">
-          <li>twitter</li>
-          <li>twitter</li>
-          <li>twitter</li>
-          <li>twitter</li>
+          <li>
+            <ExternalLink href="https://twitter.com">
+              <FaTwitter size={15} />
+            </ExternalLink>
+            <span>Twitter</span>
+          </li>
+          <li>
+            <ExternalLink href="https://discord.com">
+              <SiDiscord size={15} />
+            </ExternalLink>
+            <span>Discord</span>
+          </li>
+          <li>
+            <ExternalLink href="https://discord.com">
+              <FaGithub size={14} />
+            </ExternalLink>
+            <span>Github</span>
+          </li>
+          <li>
+            <ExternalLink href="https://discord.com">
+              <FaHeart size={14} />
+            </ExternalLink>
+            <span>Donate</span>
+          </li>
         </ul>
       </aside>
     </header>
